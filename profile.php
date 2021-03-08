79c949abffb1f198c99958cf144849c636abd4a4
Ashinindavudha <ashinindavudha2018@gmail.com>
2021-03-08 15:15:32 +0630

    php_ajax_crud_lesson_compelete

---
+++ profile.php
@@ -0,0 +1,32 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Profile</title>
+</head>
+<body>
+<!-- profile modal start -->
+    <div class="modal fade" id="userViewModal" tabindex="-1" role="dialog" aria-labelledby="userViewModalLabal" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-conten">
+            <div class="modal-header">
+            <h5 class="modal" id="exampleModalLabel">Profile <i class="fa fa-user-circle-o" aria-hidden="true"></i></h5>
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span class="" aria-hidden="true">&times;</span>
+            </button>
+            <div class="modal-body">
+            <div class="container" id="profile"></div>
+            </div>
+            <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+            </div>
+            </div>
+            </div>
+        </div>
+    </div>
+    <!-- profile modal end -->
+</body>
+</html>
