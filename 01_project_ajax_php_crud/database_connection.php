04701c6867e0480f41f61dcb763c82aca5893c6e
Ashinindavudha <ashinindavudha2018@gmail.com>
2021-03-17 15:36:01 +0630

    php_ajax_datatable_code_completed

---
+++ 01_project_ajax_php_crud/database_connection.php
@@ -0,0 +1,2 @@
+<?php
+$connection = new PDO('mysql:host=localhost; dbname=course_db', 'root', '');
---
+++ 01_project_ajax_php_crud/delete.php
@@ -0,0 +1,11 @@
+<?php
+include('database_connection.php');
+include('function.php');
+if (isset($_POST["course_id"])) {
+    $statement = $connection->prepare(
+        "DELETE FROM course WHERE id = :id"
+    );
+    $result = $statement->execute(
+        array(':id' => $_POST["course_id"])
+    );
+}
